b4500848d95ce7f8e3b331c4dd46b0d5199b78b4
kumamahesh2019 <164604202+kumamahesh2019@users.noreply.github.com>
2024-03-28 14:28:17 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,9 @@
 // Your First Program
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World"); 
       System.out.println("Hello, World");
-        System.out.println("Hello, uma");
+    
     }
 }
